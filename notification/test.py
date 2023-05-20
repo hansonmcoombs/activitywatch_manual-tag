05c5471ec749e6fab571711e2db07eed07d8018a
matt_dumont <hansonmcoombs@gmail.com>
2023-05-20 17:52:41 +1200

    test commit

---
+++ notification/test.py
@@ -0,0 +1,16 @@
+"""
+created matt_dumont 
+on: 20/05/23
+"""
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from notification.notify_on_amount import desktop_notification
+
+if __name__ == '__main__':
+    print('test notification')
+    print('PATH:')
+    print('\n * :' + '\n *: '.join(os.environ['PATH'].split(':')))
+    print('PYTHONPATH')
+    print('\n * :' + '\n *: '.join(sys.path))
+    desktop_notification('TEST', 'test notification')
